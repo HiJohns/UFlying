6f7f7a7bbc717620d4a11afd4c2ccfcdb9761359
HiJohns <linwx1978@yahoo.com>
2015-08-05 09:08:00 -0400

    First task page

--- WebContent/js/dialogs/Marriage.js
+++ WebContent/js/dialogs/Marriage.js
@@ -1,4 +1,4 @@
-UFlying.controller('marriageDialog', function ($scope, $modalInstance, cities, data) {
+UFlying.controller('marriageDialog', function ($scope, $modalInstance, cities, data, $timeout) {
     cities.load(function (data) {
         var provinces = [];
         for (var province in data) {
@@ -34,4 +34,37 @@ UFlying.controller('marriageDialog', function ($scope, $modalInstance, cities, d
     $scope.cityChange = function () {
         $scope.address = '';
     }
+
+    $scope.open = function() {
+      $timeout(function() {
+        $scope.opened = true;
+      });
+    };
+
+    $scope.dateOptions = {
+      'year-format': "'yy'",
+      'starting-day': 1,
+      'show-weeks': false,
+      'current-text': '今天',
+      'clear-text': '清空',
+      'close-text': '完成'
+    };
+
+    var _dateTemplate = _.template('<%=year%>年<%=month%>月<%=date%>日');
+
+    var date = $scope.date = new Date();
+    $scope.dateText = _dateTemplate({
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        date: date.getDate()
+    });
+
+    $scope.startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 8, 0);
+    $scope.endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 17, 0);
+
+    $scope.account = {
+        phone: '18618686888',
+        uid: 'G0000000004'
+    }
+    $scope.logined = _.isObject($scope.account);
 })
