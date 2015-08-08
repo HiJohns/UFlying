81e78822e379109e0b23f2408ac8f0d752288167
HiJohns <linwx1978@yahoo.com>
2015-08-08 00:34:02 -0400

    Add real logic for login and cities retrieval

--- WebContent/js/mission/dialogs/Login.js
+++ WebContent/js/mission/dialogs/Login.js
@@ -1,10 +1,22 @@
-UFlying.controller('dialogs.Login', function ($scope, $modalInstance, $http, data) {
+UFlying.controller('dialogs.Login', function ($scope, $modalInstance, $http, data, $cookies) {
     $scope.cancel = function () {
-        alert($scope.user);
         $modalInstance.dismiss('Canceled');
     }
 
     $scope.ok = function () {
-        $modalInstance.close();
+    	$http.post('../ajax_login', JSON.stringify({ user: $scope.user, password: $scope.password }))
+    		.then(function (response) {
+    			var data = response.data;
+    			console.log(response);
+    			if (data.status != 0) {
+    				alert('登录失败！原因如下：' + data.reason);
+    				return;
+    			}
+    			
+    			$cookies.put('token', data.account.token);
+    			$modalInstance.close(data.account);
+    		}, function (response) {
+    			alert('登录失败！');
+    		})
     }
 });
